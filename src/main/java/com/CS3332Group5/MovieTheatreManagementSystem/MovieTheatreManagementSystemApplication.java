75abd58ac053a52bc479bfd8ce3ebe2e51042961
Esvinded <phong.vnk228033@sis.hust.edu.vn>
2025-04-19 18:59:16 +0700

    Initial project setup

---
+++ src/main/java/com/CS3332Group5/MovieTheatreManagementSystem/MovieTheatreManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.CS3332Group5.MovieTheatreManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTheatreManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTheatreManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CS3332Group5/MovieTheatreManagementSystem/MovieTheatreManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CS3332Group5.MovieTheatreManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTheatreManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
