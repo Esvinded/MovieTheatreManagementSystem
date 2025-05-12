d0793ecefa07ecc415c3ad3f49e62bbddb06f0d1
Esvinded <phong.vnk228033@sis.hust.edu.vn>
2025-05-12 20:39:04 +0700

    Deleted duplicate files

--- backend/src/main/java/com/CS3332Group5/MovieTheatreManagementSystem/features/theatres/repository/ScreenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.CS3332Group5.MovieTheatreManagementSystem.features.screen.repository;
-
-import com.CS3332Group5.MovieTheatreManagementSystem.features.screen.entity.Screen;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ScreenRepository extends JpaRepository<Screen, Long> {
-}
