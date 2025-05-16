cac915c86720780fa5f7ab9a03b76b32d6d33a0b
tthvuive <tranhung.k53tincsp@gmail.com>
2025-05-16 13:39:17 +0700

    finalupdate

--- src/main/java/com/CS3332Group5/MovieTheatreManagementSystem/features/showtimes/repository/ShowtimeRepository.java
+++ src/main/java/com/CS3332Group5/MovieTheatreManagementSystem/features/showtimes/repository/ShowtimeRepository.java
@@ -2,20 +2,9 @@ package com.CS3332Group5.MovieTheatreManagementSystem.features.showtimes.reposit
 
 import com.CS3332Group5.MovieTheatreManagementSystem.features.showtimes.entity.Showtime;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.OffsetDateTime;
+import java.util.List;
 
 public interface ShowtimeRepository extends JpaRepository<Showtime, Long> {
-
-    /* các hàm đã có */
-    boolean existsByMovie_Id(Long movieId);
-    boolean existsByScreen_Id(Long screenId);
-    boolean existsByScreen_IdAndStartTimeAfter(Long screenId, OffsetDateTime time);
-
-    /* ---- hàm xoá showtime cũ ---- */
-    @Transactional
-    @Modifying              // bắt buộc với query delete/update
-    void deleteByStartTimeBefore(OffsetDateTime time);
+    List<Showtime> findByMovieIdAndStartTimeAfter(Long movieId, OffsetDateTime now);
+    List<Showtime> findByScreenIdAndStartTimeAfter(Long screenId, OffsetDateTime now);
 }
